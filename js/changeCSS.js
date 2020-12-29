a3d3c0462b95c58f60ed7767aad0e5bd2f9ef25d
Satyam Bansal <sbansal1999@gmail.com>
2020-12-29 17:33:06 +0530

    idk

---
+++ js/changeCSS.js
@@ -0,0 +1,7 @@
+function changeCSSStyle() {
+    let title = document.querySelector('#mainTitle');
+
+    title.style.color = 'black';
+    title.style.backgroundColor = 'pink';
+    title.style.border = '10px dashed blue';
+}
